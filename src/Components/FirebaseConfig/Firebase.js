bac561b59a12fa6a6827bb4b1339251c2351cfb1
Ravikiran303 <ravikiran2221@gmail.com>
2020-02-12 21:39:21 +0530

    updated firebased credentials

--- src/Components/FirebaseConfig/Firebase.js
+++ src/Components/FirebaseConfig/Firebase.js
@@ -4,10 +4,10 @@ const settings = { timestampsInSnapshots: true };
 
 var firebaseConfig = {
   apiKey: "12345",
-  authDomain: "terracetask.firebaseapp.com",
-  databaseURL: "https://terracetask.firebaseio.com",
+  authDomain: "abcd",
+  databaseURL: "example.com",
   projectId: "terracetask",
-  storageBucket: "terracetask.appspot.com",
+  storageBucket: "app.com",
   messagingSenderId: "739298815265",
   appId: "1:739298815265:web:e8909b33947197f7"
 };
